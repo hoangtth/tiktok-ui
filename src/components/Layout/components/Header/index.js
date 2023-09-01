9c4e9a24f5bc3b717a1e08e324f320d6365a170a
hoangtth <bibilove9876@gmail.com>
2023-09-01 22:13:52 +0700

    Bai63 Layout mac dinh

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,17 @@
+import classNames from 'classnames/bind';
+import styles from '~/components/Layout/components/Header/Header.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                {/* {Logo} */}
+                {/* {Search} */}
+            </div>
+        </header>
+    );
 }
 
 export default Header;
